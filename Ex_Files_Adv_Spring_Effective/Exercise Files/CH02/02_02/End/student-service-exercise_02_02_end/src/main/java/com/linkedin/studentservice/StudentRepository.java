7c604f70389250df37c4df5fc265b6cb7e46134f
EMIRATES\S442489 <shobha.kamath@emirates.com>
2021-05-05 15:11:35 +0400

    few changes

--- Ex_Files_Adv_Spring_Effective/Exercise Files/CH02/02_02/End/student-service-exercise_02_02_end/src/main/java/com/linkedin/studentservice/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.linkedin.studentservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long>
-{
-    Student getStudentByName(String name);
-}
